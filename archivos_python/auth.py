1a6a1775e829caf342219598c7493938534a332c
Cesar Rios <cesar.webdev@outlook.com>
2025-11-23 15:23:54 -0500

    Primer commit del proyecto sistema_interplanetario

---
+++ archivos_python/auth.py
@@ -0,0 +1,29 @@
+import csv
+
+# Carga al administrador desde el CSV.
+def cargarAdmin():
+    with open("archivos_CSV/admin_access.csv", "r", encoding="utf-8") as archivo:
+        reader = csv.DictReader(archivo)
+        for row in reader:
+            return row 
+        
+# Inicio de sesion validando credenciales con el csv
+def sesionAdmin(intentos = 3):
+    # Llamo la funcion cargar admin que es la del csv
+    admin = cargarAdmin()
+    # Intentos de inicio de sesion
+    if intentos <= 0:
+        print("Has agotado tus intentos")
+        exit()
+    
+    
+    print("\n--- PANEL DE INICIO DE SESION ---")
+    usuario = input("Ingrese su usuario: ").lower()
+    contrasena = input("Ingrese su contraseña: ")
+    # Valido credenciales
+    if usuario == admin["usuario"] and contrasena == admin["contrasena"]:
+        print("\nSUPERADMIN, Bienvenido al sistema")
+        return True
+    
+    print(f"\nCredenciales incorrectas. Te quedan {intentos - 1} intentos.")
+    return sesionAdmin(intentos - 1) 
---
+++ archivos_python/main.py
@@ -0,0 +1,35 @@
+from auth import sesionAdmin
+
+def main():
+    sesionAdmin()
+    print("\nSistema iniciado correctamente.")
+    
+def menu():
+    print("\n---- MENU PRINCIPAL ----")
+    print("1. Visitantes Intergalácticos")
+    print("2. Artefactos Recuperados")
+    print("3. salir del sistema")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
